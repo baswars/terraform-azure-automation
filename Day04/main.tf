e3bd97eba7a24ffb1ce80ad8836ec9f96c2fd353
sachin baswr <baswarsachin9@gmail.com>
2024-05-28 22:41:30 +0530

    added storage container in main.tf

---
+++ Day04/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">3"
+    }
+  }
+}
+provider "azurerm" {
+  features {
+
+  }
+
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-vm"
+  location = "East US"
+
+}
+resource "azurerm_storage_account" "storage-account" {
+  name                     = "storageaccountday04"
+  resource_group_name      = "rg-vm"
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  depends_on = [ azurerm_resource_group.rg ]
+
+}
+resource "azurerm_storage_container" "example" {
+  name                  = "storagecontainerday04"
+  storage_account_name  = azurerm_storage_account.storage-account.name
+  container_access_type = "private"
+}
