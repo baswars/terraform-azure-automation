bd7331378df0a8dc8ad4847ae6e3e64b6d0af2a5
Sachin.baswar <baswarsachin@gmail.com>
2024-06-19 10:27:12 +0530

    added subnet3 code in main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,10 @@ resource "azurerm_subnet" "name" {
   address_prefixes     = ["10.0.1.0/24"]
 
 }
+resource "azurerm_subnet" "subnet3" {
+  name                 = "subnet3"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+
+}
