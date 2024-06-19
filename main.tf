c7dad426f31e4efe6280fb527f006f70be8ed899
Sachin.baswar <baswarsachin@gmail.com>
2024-06-19 10:25:54 +0530

    added subnet2 code in main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,17 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
 
 }
-resource "azurerm_subnet" "name" {
+resource "azurerm_subnet" "subnet1" {
   name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+
+}
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 
 }
