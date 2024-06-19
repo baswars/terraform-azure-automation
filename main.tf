f61dfcd372cc63bbef152b86c03ef384139ee368
Sachin.baswar <baswarsachin@gmail.com>
2024-06-19 10:05:56 +0530

    added main.tf code in main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 
 }
+resource "azurerm_virtual_network" "vnet" {
+    name="vnet"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    address_space = ["10.0.0.0/16"]
+  
+}
